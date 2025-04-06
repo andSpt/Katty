a7ac29a55554e25908b8076a0293dc7d0b213b7d
andSpt <henrrry52@gmail.com>
2025-04-06 11:21:50 +0300

    del text add def multi

--- first/nums.py
+++ first/nums.py
@@ -2,13 +2,16 @@ def plus(x,y):
     return x+y
 
 
-text = 'hello'
 
 def minus(x,y):
     return x-y
+
+def multi(x,y):
+    return x*y
 
 
 
 if __name__ == '__main__':
     print(plus(1,2))
     print(minus(4,2))
+    print(multi(3,3))
